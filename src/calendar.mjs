e7e4f0f6fb42428ed4f6a540738df8f41f528e68
Karla G. <k.grajalesluna@gmail.com>
2025-06-16 21:23:15 +0100

    created dropdown button for months and years, create Table, th, tr and tbody.

--- src/calendar.mjs
+++ src/calendar.mjs
@@ -1,12 +1,17 @@
+import { Header } from "tar";
+
+//main container
 const calendarContainer = document.createElement ("div");
 calendarContainer.id = "calendar-container";
 
+//title div
 const titleDiv = document.createElement("div");
 titleDiv.id ="calendar-title";
 titleDiv.textContent ="Commemorative Days Calendar";
 
-const controlsDivs = document.createElement("div");
-controlsDivs.id = "calendar-controls";
+//controls div
+const controlsDiv = document.createElement("div");
+controlsDiv.id = "calendar-controls";
 
 const tableDiv = document.createElement("div");
 tableDiv.id = "calendar-table";
@@ -15,21 +20,62 @@ tableDiv.id = "calendar-table";
 // ----- button previous button -------
 const prevButton = document.createElement("button");
 prevButton.id = "prev-button";
-prevButton.textContent = "Previous";
+prevButton.textContent = " ← Previous";
 
 // ----- button previous button -------
 const nextButton = document.createElement("button")
 nextButton.id = "next-button";
-nextButton.textContent = "Next";
+nextButton.textContent = "Next → ";
 
-// ----- button previous button -------
-const
+// ----- Dropdown Months  -------
+const monthSelect = document.createElement("select")
+monthSelect.id = "month-select";
+
+// ----- Dropdown Years  -------
+const yearSelect = document.createElement("select");
+yearSelect.id = "year-select";
+
+
+//appended buttons and dropdown to its container.
+controlsDiv.appendChild(prevButton);
+controlsDiv.appendChild(nextButton);
+controlsDiv.appendChild(monthSelect);
+controlsDiv.appendChild(yearSelect);
+
+
+// ----- Table calendar  -------
+const table = document.createElement("table");
+table.id = "calendar-grid";
+
+// ----- header row with day names  -------
+const thead = document.createElement("thead");
+const headerRow = document.createElement("tr");
+
+const dayNames = 
+["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+dayNames.forEach(day  => {
+    const th = document.createElement("th");
+    th.textContent = day;
+    headerRow.appendChild("th")
+    
+});
+
+thead.appendChild(headerRow)
+
+// ----- body where dates will be injected dynamically  -------
+const tbody = document.createElement("tbody");
+tbody.id = "calendar-body";
+
+// ----- body where dates will be injected dynamically  -------
+table.appendChild(thead);
+table.appendChild(tbody)
+tableDiv.appendChild(table)
 
 
 calendarContainer.appendChild(titleDiv);
-calendarContainer.appendChild(controlsDivs);
+calendarContainer.appendChild(controlsDiv);
 calendarContainer.appendChild(tableDiv);
 
-
+ 
 //appended to the body the structure
 document.body.appendChild(calendarContainer);
